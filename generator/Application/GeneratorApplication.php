a299b70507a688c948e5bf98748108500c1c1295
Stefan Graupner <efrane@meanderingsoul.com>
2021-04-04 20:14:06 +0200

    feat: Setup DI

--- generator/Application/GeneratorApplication.php
+++ generator/Application/GeneratorApplication.php
@@ -4,18 +4,39 @@ declare(strict_types=1);
 
 namespace EFrane\SchemaObjects\Generator\Application;
 
+use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
+
 
 final class GeneratorApplication
 {
     public function __invoke()
     {
+        $container = $this->boot();
+
+        return $this->run($container);
+    }
+
+    /**
+     * @return int
+     * @throws \Exception
+     */
+    private function run(ContainerInterface $container): int
+    {
         $consoleApplication = new Application('generator', '-void-');
 
-        $consoleApplication->add(new GenerateCommand(new CodeGenerator()));
+        $consoleApplication->add($container->get(GenerateCommand::class));
 
         $consoleApplication->setDefaultCommand('generate');
 
         return $consoleApplication->run();
+    }
+
+    private function boot(): ContainerInterface
+    {
+        $containerBuilder = new ContainerBuilder();
+
+        return $containerBuilder->build();
     }
 }
